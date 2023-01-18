038c2b63ce9f82ab323d4efeeebc559076e0aeab
Erik Pavlin <epavlin2@gmail.com>
2023-01-18 17:02:29 +0100

    Prvi commit

---
+++ Prva_vaja/Prva_vaja/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prva_vaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdravljen.");
+            Console.Read();
+        }
+    }
+}
