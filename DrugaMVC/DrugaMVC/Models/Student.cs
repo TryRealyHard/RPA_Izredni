46bea26d3c5521e822d785f428ba2856a46d7260
Erik Pavlin <epavlin2@gmail.com>
2023-01-19 19:01:17 +0100

    Start nove aplikacije, kjer dodamo bazo na roke.. to be continued.

---
+++ DrugaMVC/DrugaMVC/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrugaMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public int Starost { get; set; }
+
+    }
+}
