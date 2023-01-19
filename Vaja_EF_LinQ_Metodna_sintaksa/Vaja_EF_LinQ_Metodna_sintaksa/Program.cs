b99d27e3c738e8176eed05061a25837546b40424
Erik Pavlin <epavlin2@gmail.com>
2023-01-19 16:34:11 +0100

    dokončana naloga 19.01.2023.

--- Vaja_EF_LinQ_Metodna_sintaksa/Vaja_EF_LinQ_Metodna_sintaksa/Program.cs
+++ Vaja_EF_LinQ_Metodna_sintaksa/Vaja_EF_LinQ_Metodna_sintaksa/Program.cs
@@ -25,19 +25,43 @@ namespace Vaja_EF_LinQ_Metodna_sintaksa
             //2. izberi čas meritve in skupno moč za datum 18.8.2013
             DateTime d1 = DateTime.Parse("18.8.2013");
             DateTime d2 = DateTime.Parse("19.8.2013");
-            var x2 = db.Meritve.Where(a => a.ZapisČas >= d1 && a.ZapisČas <= d2);
+            var x2 = db.Meritve.Where(a => a.ZapisČas >= d1 && a.ZapisČas <= d2).Take(10);
 
-            foreach(var x in x2)
+            foreach (var x in x2)
             {
                 Console.WriteLine(x.ZapisČas);
             }
+
             Console.WriteLine("----****\\|/****----");
             //3. izračunaj povprečno moč za datum 18.8.2013
-            //4. izračunaj maximalno moč za ta datum
-            //5. izračunaj minimalno moč za ta datum
-            //6. izračunaj povprečno moč po urah za dan 18.8.2013
-            //7. izračunaj 15 minutna povprečja za 18.8.2013
+            var x3 = db.Meritve.Average(e => e.kW1 + e.kW2 + e.kW3);
+            Console.WriteLine(x3);
 
+            Console.WriteLine("----****\\|/****----");
+            //4. izračunaj maximalno moč za ta datum
+            var x4 = db.Meritve.Max(e => e.kW1 + e.kW2 + e.kW3);
+            Console.WriteLine(x4);
+
+            Console.WriteLine("----****\\|/****----");
+            //5. izračunaj minimalno moč za ta datum
+            var x5 = db.Meritve.Max(e => e.kW1 + e.kW2 + e.kW3);
+            Console.WriteLine(x5);
+
+            Console.WriteLine("----****\\|/****----");
+            //6. izračunaj povprečno moč po urah za dan 18.8.2013
+            var x6 = db.Meritve.GroupBy(e => e.ZapisČas.Value.Hour).Select(e => new { Ura = e.Key, Moc = e.Average(a => a.kW1 + a.kW2 + a.kW3) });
+
+            foreach(var x in x6)
+            {
+                Console.WriteLine(x.Ura+" "+x.Moc);
+            }
+
+            Console.WriteLine("----****\\|/****----");
+            //7. izračunaj 15 minutna povprečja za 18.8.2013
+            var x7 = db.Meritve.GroupBy(e => new { ura = e.ZapisČas.Value.Hour, cetrt = e.ZapisČas.Value.Minute / 15 }).OrderBy(e => e.Key.ura).Select(e => new { ura = e.Key.ura, cetrt = e.Key.cetrt, moc = e.Average(a => a.kW1 + a.kW2 + a.kW3) });
+
+            foreach(var x in x7)
+                Console.WriteLine(x.ura+"\t"+x.cetrt+"\t"+x.moc);
             Console.ReadLine();
         }
     }
