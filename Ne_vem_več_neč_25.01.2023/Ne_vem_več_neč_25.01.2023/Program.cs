1a0a1ce8f5bc16a93bd5a3e2f336a1f67160599a
Erik Pavlin <epavlin2@gmail.com>
2023-01-25 18:59:51 +0100

    Proti koncu ure.

--- Ne_vem_več_neč_25.01.2023/Ne_vem_več_neč_25.01.2023/Program.cs
+++ Ne_vem_več_neč_25.01.2023/Ne_vem_več_neč_25.01.2023/Program.cs
@@ -10,8 +10,10 @@ namespace Ne_vem_več_neč_25._01._2023
     {
         static void Main(string[] args)
         {
-            RunAsync().wait();
+            //RunAsync().wait();
         }
         //TODO: KLONIREJ SI od drugje KER GRE PREHITRO!!!!!!!!!!!!!!!!!!
+        //https://ucilnice.scng.si/pluginfile.php/43627/mod_resource/content/0/Zacnimo_z_Web_API.pdf
+        //Domača Naloga.
     }
 }
